1ddf31ae714ff59efa5c290eaa48090c48103a83
camilo ruiz <camiloruizvidal@gmail.com>
2021-03-07 22:09:26 -0500

    separate functions

--- src/class/CarInsurance.js
+++ src/class/CarInsurance.js
@@ -1,7 +1,8 @@
 class CarInsurance {
+	#products = [];
 	constructor(products = []) {
-		this.products = products;
-		console.log('products', this.products);
+		this.#products = products;
+		console.log('products', this.#products);
 	}
 	addProduct(product) {
 		this.products.push(product);
@@ -12,58 +13,85 @@ class CarInsurance {
 	updateProduct(index, product) {
 		this.products[index] = product;
 	}
-	updatePrice() {
-		this.products = this.products.map(product => {
-			if (
-				![
-					'Full Coverage',
-					'Mega Coverage',
-					'Special Full Coverage'
-				]
-				.includes(product.name) &&
-				product.price > 0
-			) {
-				product.price = product.price - 1;
-			} else {
-				if (product.price < 50) {
-					product.price = product.price + 1;
 
-					if (product.name == 'Special Full Coverage') {
-						if (product.sellIn < 11 && product.price < 50) {
-							product.price = product.price + 1;
-						}
-						if (product.sellIn < 6) {
-							if (product.price < 50) {
-								product.price = product.price + 1;
-							}
-						}
-					}
-				}
-			}
-
-			if (product.name != 'Mega Coverage') {
-				product.sellIn = product.sellIn - 1;
-			}
+	#updateProductFullCoverage(product) {
+		product.sellIn = product.sellIn - 1;
+		if (product.price < 50) {
 			if (product.sellIn < 0) {
-				if (product.name != 'Full Coverage') {
-					if (product.name != 'Special Full Coverage') {
-						if (product.price > 0) {
-							if (product.name != 'Mega Coverage') {
-								product.price = product.price - 1;
-							}
-						}
-					} else {
-						product.price = product.price - product.price;
-					}
-				} else {
-					if (product.price < 50) {
-						product.price = product.price + 1;
-					}
-				}
+				product.price = product.price + 2;
+			} else {
+				product.price = product.price + 1;
 			}
-			return product;
-		})
-		return this.products;
+		}
+		return product;
+	}
+
+	#updateProductMegaCoverage(product) {
+		product.price = 80;
+		return product;
+	}
+
+	#updateProductSpecialFullCoverage(product) {
+		if (product.sellIn < 0) {
+			product.price = 0;
+		} else {
+			if (product.sellIn > 5 && product.sellIn <= 10) {
+				product.price = product.price + 2;
+			} else if (product.sellIn <= 5) {
+				product.price = product.price + 3;
+			} else {
+				product.price = product.price + 1;
+			}
+			if (product.price > 50) {
+				product.price = 50;
+			}
+		}
+		product.sellIn = product.sellIn - 1;
+		return product;
+	}
+
+	#updateProductSuperSale(product) {
+		product.sellIn = product.sellIn - 1;
+		if (product.price < 50 && product.price >0) {
+			product.price = product.price - 1;
+		}
+		return product;
+	}
+
+	#updateProductOtherCoverage(product) {
+		product.price = product.price - 1;
+		product.sellIn = product.sellIn - 1;
+		if (product.sellIn < 0 && product.price > 0) {
+			product.price = product.price - 1;
+		}
+		if (product.price < 0) {
+			product.price = 0;
+		}
+		return product
+	}
+
+	updatePrice() {
+		this.#products = this.#products.map(product => {
+				switch (product.name) {
+					case 'Full Coverage':
+						product = this.#updateProductFullCoverage(product);
+					break;
+					case 'Mega Coverage':
+						product = this.#updateProductMegaCoverage(product);
+					break;
+					case 'Special Full Coverage':
+						product = this.#updateProductSpecialFullCoverage(product);
+					break;
+					case 'Super Sale':
+						product = this.#updateProductSuperSale(product);
+					break;
+					default:
+						product = this.#updateProductOtherCoverage(product);
+					break;
+				}
+				return product;
+		});
+		return this.#products;
 	}
 }
 module.exports = CarInsurance
